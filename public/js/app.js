1220da6b49371ccab1232902e9102f908ed03d11
Julio Scarpelini <julio@santa.io>
2017-03-26 11:02:18 -0300

    Added complete flow

--- public/js/app.js
+++ public/js/app.js
@@ -1,64 +1,100 @@
 angular.module('Medibot', [])
-  .controller('MedibotController', ['MedibotService', function(MedibotService) {
+  .controller('MedibotController', ['MedibotService', '$window', function(MedibotService, $window) {
 
   	console.log("Medibot Controller  >>>>>");
   	console.log("Medibot Service  >>>>>", MedibotService);
     
     var vm = this;
+    var currentContext = null;
 
     vm.textFieldValue;
-    
-    vm.messages = [
-      {messageClass:"myself-message", message:"Olááááá enfermeeeira!"},
-      {messageClass:"myself-message", message:"Estou com dores no meu coração. Acho que é paixão <3"},
-      {messageClass:"bot-message", message:"Jura? Tenho um ótimo remédio pra você."},
-      {messageClass:"bot-message", message:"VODKA!"},
+    vm.choiceOptions = [
+    // 	{"id":"asasasasasasas", "description":"Perda de sensibilidade"}, 
+	  	// {"id":"sdsdsdsdsdsdsd", "description":"Inchaço"},
+	  	// {"id":"fgfgfgfgfgfgfg", "description":"Formigamento nas mãos"} 
     ];
     
-    vm.chooseOption = function(code) 
+    vm.messages = [
+      // {messageClass:"myself-message", message:"Olááááá enfermeeeira!"},
+      // {messageClass:"myself-message", message:"Estou com dores no meu coração. Acho que é paixão <3"},
+      // {messageClass:"bot-message", message:"Jura? Tenho um ótimo remédio pra você."},
+      // {messageClass:"bot-message", message:"VODKA!"},
+    ];
+    
+    vm.chooseOption = function(code, label) 
     {
-    	MedibotService.postOptionsChoice(code).then(function(e){
-    		console.log("Sended Option Choice - Response :: ", e);
+    	console.log("Option Choice :: ", code);
+    	console.log("Option Label :: ", label);
 
-    		vm.messages.push({messageClass:"bot-message", message:message});
-    	});  
+    	showMessage("bot", label);
+    	vm.choiceOptions = [];
+
+    	MedibotService.postOptionsChoice(code, currentContext).then(sucessHandler);  
     }
 
-    vm.sendMessage = function() 
+    vm.sendMessage = function(start) 
     {
-    	var message = vm.textFieldValue;
+    	var message = start ? "call" : vm.textFieldValue;
 
-    	console.log("vm.textFieldValue :: ", message);
-    	// return;
+    	if(message == "") return;
 
-      	showMessage("myself", message);
-     //  	MedibotService.postMessage(message).then(function(e){
-
-     //  		var response = "Resposta de marcação.";
-     //  		var options = {};
-
-     //  		showMessage("bot", message, options);
-    	// 	console.log("Sended Message - Response :: ", e);
-    	// });  
+      	if(message != "call") showMessage("myself", message);
+      	vm.textFieldValue = "";
+      	MedibotService.postMessage(message).then(sucessHandler);  
     };
+
+    var sucessHandler = function(e)
+    {
+		console.log("Success Handler - Response :: ", e);
+
+		var message = e.data.message;
+
+		currentContext = e.data.context;
+  		vm.choiceOptions = e.data.symptons;
+
+		showMessage("bot", message);
+    }
 
     function showMessage(type, message, options)
     {
     	var messageType = type+"-message";
     	vm.messages.push({messageClass:messageType, message:message});
+
+    	var messagesContainer = $("#messages-container");
+    	var messagesContainerHeight = messagesContainer.height();
+    	messagesContainer.scrollTop(messagesContainerHeight-100);
     }
 
-  }]).service('MedibotService', ['$http', function($http){
-  	var service = this;
+    // start
+    vm.sendMessage(true);
 
-  	service.postMessage = function(message)
+  }]).service('MedibotService', ['$http', function($http){
+  	
+  	var service = this;
+  	var production = true;
+  	var serviceURL = 'http://10.18.240.81:8080/';
+
+  	var sendMessageURLService = production ? serviceURL : 'data/post_message.json';
+  	var sendOptionsURLService = production ? serviceURL : 'data/post_choice.json';
+
+  	service.postMessage = function(message, context)
   	{
-  		return $http.post('data/post_message.json', message, config).then(successMessageHandler, errorMessageHandler);
+  		var data = {
+		  "message": message,
+		  "context": context
+		}
+
+  		return $http.post(sendMessageURLService, data);
   	}
 
-  	service.postOptionsChoice = function(code)
+  	service.postOptionsChoice = function(code, context)
   	{
-  		return $http.post('data/post_choice.json', code, config).then(successMessageHandler, errorMessageHandler);
+  		var data = {
+		  "symptons": code,
+		  "context": context
+		}
+
+  		return $http.post(sendOptionsURLService, data);
   	}
 
   	function successMessageHandler(e)
